04619e5b7032c5c37e24cd5c8e516a121e9c0662
Bardak333 <adima20031@yandex.ru>
2025-09-19 13:14:52 +0500

    Bl

---
+++ script3.js
@@ -0,0 +1,25 @@
+const header__wraper = document.querySelector('.header__wraper');
+const btn__openMenu = document.querySelector('.header__menu');
+const header__aplication = document.querySelector('.header__application');
+const header = document.querySelector('.header');
+const container = document.querySelector('.container');
+const mobile__menu = document.querySelector('.header__mobile__menu');
+
+function open_close__menu() {
+    header.classList.toggle('header-open');
+    container.classList.toggle('container-open');
+    header__aplication.classList.toggle('header__aplication-open');
+    header__wraper.classList.toggle('header__wraper-open');
+    btn__openMenu.classList.toggle('header__menu-open');
+    // mobile__menu.classList.toggle('header__mobile__menu-open');
+
+    if (mobile__menu.style.display === 'flex') {
+        mobile__menu.style.display = 'none';
+    }
+    else {
+        mobile__menu.style.display = 'flex';
+    }
+
+}
+
+btn__openMenu.addEventListener('click', open_close__menu);

0c7d813b1ba5236b3b610c6b48f025ebcb4745c7
Bardak333 <adima20031@yandex.ru>
2025-09-25 16:07:04 +0500

    B1

--- script3.js
+++ script3.js
@@ -67,6 +67,9 @@ let case_count = 0;
 const case__scroller_btn_back = document.querySelector(".case__scroller-btn_back");
 const case__scroller_btn_next = document.querySelector(".case__scroller-btn_next");
 
+const case__slider_back_btn = document.querySelector('.case__slider-back_btn');
+const case__slider_next_btn = document.querySelector('.case__slider-next_btn');
+
 function next_case () {
     if (case_count >= 0 && case_count < 7) {
         case_counter_btn[case_count].classList.toggle('case__counter-btn_active');
@@ -108,10 +111,10 @@ case_counter_btn.forEach ((btn, index) => {
 });
 
 
-
-
 btn__openMenu.addEventListener('click', open_close__menu);
 window.addEventListener("scroll", skull_animation);
 skull_animation();
 case__scroller_btn_next.addEventListener('click', next_case);
 case__scroller_btn_back.addEventListener('click', back_case);
+case__slider_back_btn.addEventListener('click', back_case);
+case__slider_next_btn.addEventListener('click', next_case); 
